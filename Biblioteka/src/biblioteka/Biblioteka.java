14a027712d1548216bc01f948716d7201bf721bd
iva28 <iva.stanisic01@yahoo.com>
2023-03-21 09:43:25 +0100

    Napisan javadoc za klase Biblioteka i BibliotekaInterfejs

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -5,22 +5,30 @@ import java.util.List;
 
 import biblioteka.interfejs.BibliotekaInterfejs;
 
-// Ova klasa predstavlja Biblioteku u kojoj se mogu dodati knjige
-// brisati knjige i pretrazivati
+/**
+ * Implementira biblioteku, pri cemu je lista sa knjigama implementirana preko
+ * ArrayList klase
+ * 
+ * @author Iva Stanisic
+ * @since 0.9.0
+ *
+ */
 public class Biblioteka implements BibliotekaInterfejs {
-
+	
+	/**
+	 * Lista sa knjigama iz biblioteke - ArrayList implementacija
+	 */
 	private List<Knjiga> knjige = new ArrayList<Knjiga>();
-	
-	
-	//Pravi komentar, pravi kod
+
+	// Pravi komentar, pravi kod
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		if (knjiga == null)
 			throw new NullPointerException("Knjiga ne sme biti null");
-		
+
 		if (knjige.contains(knjiga))
 			throw new IllegalArgumentException("Knjiga je duplikat");
-		
+
 		knjige.add(knjiga);
 	}
 
@@ -38,14 +46,14 @@ public class Biblioteka implements BibliotekaInterfejs {
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
 		if (autor == null && isbn < 0 && naslov == null && izdavac == null)
 			throw new IllegalArgumentException("Morate uneti bar neki kriterijum za pretragu");
-		
+
 		List<Knjiga> rezultati = new ArrayList<Knjiga>();
-	
-		if (naslov!=null)
-		  for (Knjiga k: knjige)
-			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
-				rezultati.add(k);
-		
+
+		if (naslov != null)
+			for (Knjiga k : knjige)
+				if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
+					rezultati.add(k);
+
 		return rezultati;
 	}
 
--- Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -5,15 +5,64 @@ import java.util.List;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Predstavlja bublioteku u koju se mogu dodati knjige, brisati i pretrazivati.
+ * 
+ * Nacin implementacije linijske strukture sa knjigama zavisi od implementacione
+ * klase.
+ * 
+ * @author Iva Stanisic
+ * @since 0.9.0
+ *
+ */
 public interface BibliotekaInterfejs {
-	
+	/**
+	 * Dodaje knjigu u biblioteku.
+	 * 
+	 * Knjiga ne sme biti null niti duplikat - ne sme postojati ista knjiga.
+	 * 
+	 * @param knjiga nova knjiga koju treba dodati u biblioteku
+	 * 
+	 * @throws NullPointerException     ako je uneta knjiga null
+	 * @throws IllegalArgumentException ako ista knjiga vec postoji u biblioteci
+	 * 
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
-	
+
+	/**
+	 * Brise knjigu iz biblioteke.
+	 * 
+	 * Ako je uneta knjiga null ili ne postoji u biblioteci, ne desava se nista.
+	 * 
+	 * @param knjiga knjiga koju treba obrisati iz biblioteke
+	 */
 	public void obrisiKnjigu(Knjiga knjiga);
-	
+
+	/**
+	 * Vraca sve knjige iz biblioteke.
+	 * 
+	 * @return lista sa svim knjigama iz biblioteke
+	 */
 	public List<Knjiga> vratiSveKnjige();
-	
-	public List<Knjiga> pronadjiKnjigu(Autor autor, 
-			long isbn, String naslov, String izdavac);
+
+	/**
+	 * Pretrazuje biblioteku i vraca sve knjige koje odgovaraju kriterijumima pretrage.
+	 * 
+	 * Pretraga se moze vrsiti oreko autora, isbn broja, dela naslova i dela naziva izdavaca.
+	 * Pretraga preko autora i isbn broja je tacno kako je uneto, 
+	 * dok se pretrazuje prema delu naslova ili delu naslova izdavaca.
+	 * 
+	 * Ako se umesto parametra unese null ili -1 (isbn), taj kriterijum se ne koristi u pretrazi.
+	 * 
+	 * @param autor autor knjige ili nulla ko se ne pretrazuje po autoru
+	 * @param isbn isbn broj ili -1 ako se ne pretrazuje po isbn broju
+	 * @param naslov deo naslova ili null ako se ne pretrazuje po naslovu
+	 * @param izdavac deo naziva izdavaca ili null ako se ne pretrazuje po izdavacu
+	 * 
+	 * @return lista sa knjigama koje odgovaraju kriterijumima pretrage
+	 * 
+	 * @throws IllegalArgumentException ako nije unet nijedan kriterijum za pretragu
+	 */
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac);
 
 }
